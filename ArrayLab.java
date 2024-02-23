f0cef116b0777b5f015cbd0a0a52b2b889a886e3
Wally1605 <156972129+Wally1605@users.noreply.github.com>
2024-02-23 12:32:27 -0500

    Update ArrayLab.java

--- ArrayLab.java
+++ ArrayLab.java
@@ -8,38 +8,40 @@ public class Main {
 
         double input = 0;
         int count = 0;
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             nums[i] = 0;
         }
 
-        for(int i = 0; i < nums.length; i++) {
-                System.out.println("Enter "+i+" element (Enter 9999 to stop): ");
-                input = sc.nextDouble();
-                if(input == 9999){
-                    break;
-                }else {
-                    nums[i] = input;
-                    count++;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("Enter " + i + " element (Enter 9999 to stop): ");
+            input = sc.nextDouble();
+            if (input == 9999) {
+                break;
+            } else {
+                nums[i] = input;
+                count++;
+            }
 
         }
         double newNums[] = new double[count];
 
-        for(int i = 0; i < newNums.length; i++) {
+        for (int i = 0; i < newNums.length; i++) {
             newNums[i] = nums[i];
         }
 
         count = 0;
-        for(int i = 0; i < newNums.length; i++) {
-            System.out.print(newNums[i]+" ");
-            if(newNums[i] == 0){
+        for (int i = 0; i < newNums.length; i++) {
+            System.out.print(newNums[i] + " ");
+
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
                 count++;
             }
-            if(count == newNums.length){
+            if (count == nums.length - 1) {
                 System.out.println();
                 System.out.print("Error, no user numbers!");
             }
-
         }
     }
 }
